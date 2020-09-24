565adda8ffe6ad4756f370c4f8583b5a29a30c9f
Konstantin <konstantin@revimedia.com>
2020-09-24 19:20:21 +0300

    Add config, properties

--- src/main/java/guru/springframework/repositories/ProductRepository.java
+++ src/main/java/guru/springframework/repositories/ProductRepository.java
@@ -1,10 +1,11 @@
 package guru.springframework.repositories;
 
 import guru.springframework.domain.Product;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.repository.CrudRepository;
 
 /**
  * Created by jt on 1/10/17.
  */
-public interface ProductRepository extends CrudRepository<Product, String> {
+public interface ProductRepository extends MongoRepository<Product, String> {
 }
---
+++ src/main/java/guru/springframework/services/MongoDbConfig.java
@@ -0,0 +1,31 @@
+package guru.springframework.services;
+
+
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+@Configuration
+@EnableMongoRepositories(basePackages = "guru.springframework.repositories")
+public class MongoDbConfig {
+
+    @Bean
+    public MongoClient mongo() {
+        ConnectionString connectionString = new ConnectionString("mongodb://localhost:27017/mongo-demo");
+        MongoClientSettings mongoClientSettings = MongoClientSettings.builder()
+                .applyConnectionString(connectionString)
+                .build();
+
+        return MongoClients.create(mongoClientSettings);
+    }
+
+    @Bean
+    public MongoTemplate mongoTemplate() throws Exception {
+        return new MongoTemplate(mongo(), "mongo-demo");
+    }
+}
