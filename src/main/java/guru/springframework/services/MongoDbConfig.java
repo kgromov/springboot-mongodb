fadb4d850bbc8e9d9abe050fa7abb9bf873286b9
Konstantin <konstantin@revimedia.com>
2020-10-01 20:47:13 +0300

    The easiset way to work with mongo

--- src/main/java/guru/springframework/services/MongoDbConfig.java
+++ src/main/java/guru/springframework/services/MongoDbConfig.java
@@ -5,18 +5,31 @@ import com.mongodb.ConnectionString;
 import com.mongodb.MongoClientSettings;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-@Configuration
+/**
+ * Works as is without config; could be configured by application.properties
+ * Another way - extends AbstractMongoClientConfiguration
+ * But it seems redundant - required to override getDatabaseName, getMappingBasePackages - aka @ComponentScan
+ * and mongoClient
+ */
+//@Configuration
 @EnableMongoRepositories(basePackages = "guru.springframework.repositories")
 public class MongoDbConfig {
+    @Value("${spring.data.mongodb.database}")
+    private String mongoDbName;
+
+    @Value("${spring.data.mongodb.uri}")
+    private String mongoDbUri;
 
     @Bean
     public MongoClient mongo() {
-        ConnectionString connectionString = new ConnectionString("mongodb://localhost:27017/mongo-demo");
+//        MongoClients.create();
+        ConnectionString connectionString = new ConnectionString(mongoDbUri);
         MongoClientSettings mongoClientSettings = MongoClientSettings.builder()
                 .applyConnectionString(connectionString)
                 .build();
@@ -25,7 +38,7 @@ public class MongoDbConfig {
     }
 
     @Bean
-    public MongoTemplate mongoTemplate() throws Exception {
-        return new MongoTemplate(mongo(), "mongo-demo");
+    public MongoTemplate mongoTemplate(MongoClient mongoClient) throws Exception {
+        return new MongoTemplate(mongoClient, mongoDbName);
     }
 }
---
+++ src/test/java/guru/springframework/MongoTemplateTest.java
@@ -0,0 +1,36 @@
+package guru.springframework;
+
+import com.mongodb.BasicDBObjectBuilder;
+import com.mongodb.DBObject;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+@DataMongoTest
+@RunWith(SpringRunner.class)
+public class MongoTemplateTest {
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    @Test
+    public void test() {
+        // given
+        DBObject objectToSave = BasicDBObjectBuilder.start()
+                .add("key", "value")
+                .get();
+
+        // when
+        mongoTemplate.save(objectToSave, "collection");
+
+        // then
+        List<DBObject> collection = mongoTemplate.findAll(DBObject.class, "collection");
+    }
+}
